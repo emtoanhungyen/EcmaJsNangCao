bca89ef3593480d726ab5c454e0c2574d276129e
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-01-20 23:57:00 +0700

    lab3 cua em ne thay oi

---
+++ lab3/main.js
@@ -0,0 +1,36 @@
+import Navigo from "navigo";
+import newAdd from "./admin/add";
+import detailNews from "./admin/detailNews";
+import newList from "./admin/newList";
+import header from "./pages/header";
+import home from "./pages/home";
+import signin from "./pages/signin";
+import signup from "./pages/signup";
+
+const router = new Navigo("/", { linksSelector: "a" });
+
+const print = (content) => {
+    document.getElementById("menu").innerHTML = header.render();
+    document.getElementById("lab3").innerHTML = content;
+};
+router.on({
+    "/": () => {
+        print(home.render());
+    },
+    "/signin": () => {
+        print(signin.render());
+    },
+    "/signup": () => {
+        print(signup.render());
+    },
+    "/admin/news": () => {
+        print(newList.render());
+    },
+    "/admin/news/add": () => {
+        print(newAdd.render());
+    },
+    "/admin/news/:id": ({ dataz }) => {
+        print(detailNews.render(dataz.id));
+    },
+});
+router.resolve();
