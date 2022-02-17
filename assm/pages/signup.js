8e78d4e254863809b002b1ad5b0021acdf5149b2
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-02-17 13:53:42 +0700

    lam den dang ki

--- assm/pages/signup.js
+++ assm/pages/signup.js
@@ -1,18 +1,18 @@
 const signup = {
-    render() {
-        return /* html */ `
+  render() {
+    return /* html */ `
         <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div class="max-w-md w-full space-y-8">
             <div>
               <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
               <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Đăng ký</h2>
             </div>
-            <form class="mt-8 space-y-6" action="#" method="POST">
+            <form class="mt-8 space-y-6" action="#" method="POST" id="formSignup">
               <input type="hidden" name="remember" value="true">
               <div class="rounded-md shadow-sm -space-y-px">
                 <div>
                   <label for="email-address" class="sr-only">Email address</label>
-                  <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                  <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
                 </div>
                 <div>
                   <label for="password" class="sr-only">Password</label>
@@ -21,7 +21,7 @@ const signup = {
               </div>
         
               <div>
-                <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <button class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   <span class="absolute left-0 inset-y-0 flex items-center pl-3">
                     <!-- Heroicon name: solid/lock-closed -->
                     <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -35,6 +35,16 @@ const signup = {
           </div>
       </div>
         `
-    },
+  },
+  afterRender() {
+    const formSignup = document.querySelector('#formSignup');
+    formSignup.addEventListener('submit', (e) => {
+      e.preventDefault();
+      signup({
+        email: document.querySelector('#email').value,
+        password: document.querySelector('#password').value,
+      })
+    })
+  }
 };
 export default signup;
