d34833462fc14c2b4066b70b50c6af40b47e9cd6
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-03-08 15:56:17 +0700

    ket thuc buoi 1

--- web503/src/app.js
+++ web503/src/app.js
@@ -1,24 +1,50 @@
-const http = require("http");
+const express = require("express");
+const cors = require("cors");
 
-const server = http.createServer((req, res) => {
-    const { url } = req;
-    console.log(url);
-    if (url === "/products") {
-        const data = [
-            { id: 1, name: "Product A" },
-            { id: 2, name: "Product B" },
-        ];
-        res.end(JSON.stringify(data));
-    } else if (url === "/post") {
-        console.log("API Post");
+const app = express();
+app.use(cors());
+
+const checkAuth = (req, res, next) => {
+    const isAdmin = true;
+    if (isAdmin) {
+        next();
     } else {
-        res.setHeader("Content-type", "text/html");
-        res.write("<html><body> <h2>Home page</h2> </body></html>");
-        res.end();
+        console.log("KUT NGAY");
     }
+};
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+});
+app.get("/products", checkAuth, (req, res) => {
+    const data = [
+        { id: 1, name: "Product A" },
+        { id: 2, name: "Product B" },
+    ];
+    res.json(data);
 });
 
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//     const { url } = req;
+//     console.log(url);
+//     if (url === "/products") {
+//         const data = [
+//             { id: 1, name: "Product A" },
+//             { id: 2, name: "Product B" },
+//         ];
+//         res.end(JSON.stringify(data));
+//     } else if (url === "/post") {
+//         console.log("API Post");
+//     } else {
+//         res.setHeader("Content-type", "text/html");
+//         res.write("<html><body> <h2>Home page</h2> </body></html>");
+//         res.end();
+//     }
+// });
+
 const PORT = 3001;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server của bạn đang chạy cổng: ", PORT);
 });
