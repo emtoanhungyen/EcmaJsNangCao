bbb88a31247adf28b5339e174586bd46c55ce4a0
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-03-26 13:48:26 +0700

    feat: dang lam dang nhap

--- web502/src/pages/Signin.tsx
+++ web502/src/pages/Signin.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
+type SigninProps = {
+    email: string,
+    password: string,
+    onSignin: (user: InputForm) => void
+}
+type InputForm = {
+    email: string,
+    password: string
+}
 const Signin = () => {
+    const {register, handleSubmit, formState:{errors}} = useForm<InputForm>();
+    const ondSubmit: SubmitHandler<InputForm> = data => {
+        console.log(data);  
+        
+    }
   return (
     <div>
         <div>Đăng Nhập</div>
-        <form action="">
+        <form onSubmit={handleSubmit(ondSubmit)}>
             <div>
                 <label>Email</label>
-                <input type="email" placeholder="Email..."/>
+                <input type="email" placeholder="Email..." {...register('email')}/>
             </div>  
             <div>
                 <label>Password</label>
-                <input type="password" placeholder="Password..."/>
+                <input type="password" placeholder="Password..." {...register('password')}/>
             </div>  
             <button>Đăng Nhập</button>
         </form>
